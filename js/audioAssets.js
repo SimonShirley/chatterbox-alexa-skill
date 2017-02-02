8f8d56d288b25c3078d2fe9719151dd28a487b0d
Simon Shirley <email@simonshirley.uk>
2017-02-02 16:22:02 +0000

    Replaced the sample audio asset Removed sample audio asset data with a live Chatterbox episode

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -1,15 +1,20 @@
 'use strict';
 
-// Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
-var audioData = [
-    {
-        'title' : 'Episode 140',
-        'url' : 'https://feeds.soundcloud.com/stream/275202399-amazon-web-services-306355661-amazon-web-services.mp3'
-    },
-    {
-        'title' : 'Episode 139',
-        'url' : 'https://feeds.soundcloud.com/stream/274166909-amazon-web-services-306355661-aws-podcast-episode-139.mp3'
-    }
-];
+// Copy the jPlayer file list from the listen page
+var chatterboxAudioData = {
+    'chatterbox': [
+        {
+            'edition' : 1943,
+            'recorded' : '2017-01-19',
+            'url' : 'http://www.cbtn.org.uk/listen-now/chatterbox-norwich-evening-news/19th-january-2017-edition-1943/',
+            'tracks' : [
+                { title: 'Track 1 - Introduction &amp; News with Rupert &amp; Sarah', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000001.mp3' }, { title: 'Track 2 - Letters', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000002.mp3' }, { title: 'Track 3 - News', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000003.mp3' }, { title: 'Track 4 - First Feature: &#039;Bullard&#039;s Brewery&#039; read by Chris Speake', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000004.mp3' }, { title: 'Track 5 - News &amp; Closing Announcement to Part 1', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000005.mp3' }, { title: 'Track 6 - Information Desk with Marianne &amp; John', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000006.mp3' }, { title: 'Track 7 - News', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000007.mp3' }, { title: 'Track 8 - Second Feature: What&#039;s On with Margaret Thompson', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000008.mp3' }, { title: 'Track 9 - Third Feature: &#039;Trams&#039; read by Chris Speake', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000009.mp3' }, { title: 'Track 10 - News &amp; Closing Announcement to Part 2', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000010.mp3' }
+            ]
+        }
+    ],
+    'chataround': [{
 
-module.exports = audioData;
+    }]
+};
+
+module.exports = chatterboxAudioData;
