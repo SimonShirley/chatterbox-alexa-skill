47c636ef3356615ace958c3453983fe2917f5b23
Simon Shirley <email@simonshirley.uk>
2017-02-03 18:19:38 +0000

    Changed links from Chatterbox to Amazon S3 The AudioPlayer will only play content transmitted over SSL.

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -5,10 +5,19 @@ var chatterboxAudioData = {
     'chatterbox': [
         {
             'edition' : 1943,
-            'recorded' : '2017-01-19',
+            'recorded' : '2017-01-26',
             'url' : 'http://www.cbtn.org.uk/listen-now/chatterbox-norwich-evening-news/19th-january-2017-edition-1943/',
             'tracks' : [
-                { title: 'Track 1 - Introduction &amp; News with Rupert &amp; Sarah', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000001.mp3' }, { title: 'Track 2 - Letters', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000002.mp3' }, { title: 'Track 3 - News', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000003.mp3' }, { title: 'Track 4 - First Feature: &#039;Bullard&#039;s Brewery&#039; read by Chris Speake', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000004.mp3' }, { title: 'Track 5 - News &amp; Closing Announcement to Part 1', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000005.mp3' }, { title: 'Track 6 - Information Desk with Marianne &amp; John', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000006.mp3' }, { title: 'Track 7 - News', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000007.mp3' }, { title: 'Track 8 - Second Feature: What&#039;s On with Margaret Thompson', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000008.mp3' }, { title: 'Track 9 - Third Feature: &#039;Trams&#039; read by Chris Speake', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000009.mp3' }, { title: 'Track 10 - News &amp; Closing Announcement to Part 2', mp3: 'http://www.cbtn.org.uk/assets/listen-now/new-listen-now-page-2-238/tracks/00000010.mp3' }
+                { title: 'Track 1 - Introduction &amp; News with Rupert &amp; Sarah', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1943+-+19th+January+2017/00000001.mp3' },
+                { title: 'Track 2 - Letters', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1943+-+19th+January+2017/00000002.mp3' },
+                { title: 'Track 3 - News', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1943+-+19th+January+2017/00000003.mp3' },
+                { title: 'Track 4 - First Feature: &#039;Bullard&#039;s Brewery&#039; read by Chris Speake', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1943+-+19th+January+2017/00000004.mp3' },
+                { title: 'Track 5 - News &amp; Closing Announcement to Part 1', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1943+-+19th+January+2017/00000005.mp3' },
+                { title: 'Track 6 - Information Desk with Marianne &amp; John', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1943+-+19th+January+2017/00000006.mp3' },
+                { title: 'Track 7 - News', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1943+-+19th+January+2017/00000007.mp3' },
+                { title: 'Track 8 - Second Feature: What&#039;s On with Margaret Thompson', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1943+-+19th+January+2017/00000008.mp3' },
+                { title: 'Track 9 - Third Feature: &#039;Trams&#039; read by Chris Speake', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1943+-+19th+January+2017/00000009.mp3' },
+                { title: 'Track 10 - News &amp; Closing Announcement to Part 2', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1943+-+19th+January+2017/00000010.mp3' }
             ]
         }
     ],
