e4011880e1c46d332f91142178aa65b18364a765
Simon Shirley <email@simonshirley.uk>
2017-02-09 20:19:24 +0000

    Added this week's episode

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -4,6 +4,22 @@
 var chatterboxAudioData = {
     'chatterbox': [
         {
+            'edition' : 1945,
+            'recorded' : '20170209',
+            'url' : 'http://www.cbtn.org.uk/listen-now/chatterbox-norwich-evening-news/9th-february-2017-edition-1945/',
+            'tracks' : [
+                { title: 'Track 1 - Introduction &amp; News with Alison &amp; Robin', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1945+-+9th+February+2017/00000001.mp3' },
+                { title: 'Track 2 - Letters', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1945+-+9th+February+2017/00000002.mp3' },
+                { title: 'Track 3 - News', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1945+-+9th+February+2017/00000003.mp3' },
+                { title: 'Track 4 - First Feature: &#039;Tenor Horn&#039; read by Sarah Houghton', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1945+-+9th+February+2017/00000004.mp3' },
+                { title: 'Track 5 - News &amp; Closing Announcement to Part 1', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1945+-+9th+February+2017/00000005.mp3' },
+                { title: 'Track 6 - Introduction to Part 2 and Information Desk with Jane &amp; David', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1945+-+9th+February+2017/00000006.mp3' },
+                { title: 'Track 7 - News', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1945+-+9th+February+2017/00000007.mp3' },
+                { title: 'Track 8 - Second Feature: &#039;Football Round-up&#039; with Chris &amp; Rob', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1945+-+9th+February+2017/00000008.mp3' },
+                { title: 'Track 9 - News &amp; Closing Announcement to Part 2', mp3: 'https://s3.eu-west-2.amazonaws.com/cbtn/Chatterbox/Chatterbox+Edition+1945+-+9th+February+2017/00000009.mp3' }
+            ],
+        },
+        {
             'edition' : 1943,
             'recorded' : '20170126',
             'url' : 'http://www.cbtn.org.uk/listen-now/chatterbox-norwich-evening-news/19th-january-2017-edition-1943/',
